823321789516aeb9606c9471d93fbd18d1cc7799
akari1017 <98454251+akari1017@users.noreply.github.com>
2022-02-24 12:39:51 +0900

    ユーザー管理機能の単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,21 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :birth_date,      presence: true
 
+  with_options presence: true do
+    # ひらがな、カタカナ、漢字のみ許可する
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    # カタカナのみ許可する
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    # 半角英字数字のみ許可する
+    validates :password, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+    validates :password_confirmation, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
   has_many :items
   has_many :purchased_items
 end
