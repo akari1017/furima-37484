493aa39777ba6f6791dfc39f1cffd997598c18e9
akari1017 <98454251+akari1017@users.noreply.github.com>
2022-02-25 17:56:38 +0900

    ActiveHushの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :ship_fee
+  belongs_to :region
+  belongs_to :ship_time
   has_one_attached :image
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 , , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , , message: "can't be blank"}
+  validates :ship_fee_id, numericality: { other_than: 1 , , message: "can't be blank"}
+  validates :region_id, numericality: { other_than: 1 , , message: "can't be blank"}
+  validates :ship_time_id, numericality: { other_than: 1 , , message: "can't be blank"}
 end
