bb9c16ed4d9ecb5421493c9be4dbbdb970a3c79d
akari1017 <98454251+akari1017@users.noreply.github.com>
2022-02-25 18:54:56 +0900

    ルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
 
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:description,:category_id,:status_id,:ship_fee_id,:region_id,:ship_time_id,:price)
   end
   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -17,9 +17,9 @@ class Item < ApplicationRecord
   validates :price,        presence: true
 
   #ジャンルの選択が「--」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 1 , , message: "can't be blank"}
-  validates :status_id, numericality: { other_than: 1 , , message: "can't be blank"}
-  validates :ship_fee_id, numericality: { other_than: 1 , , message: "can't be blank"}
-  validates :region_id, numericality: { other_than: 1 , , message: "can't be blank"}
-  validates :ship_time_id, numericality: { other_than: 1 , , message: "can't be blank"}
+  #validates :category_id, numericality: { other_than: 1 , , message: "can't be blank"}
+  #validates :status_id, numericality: { other_than: 1 , , message: "can't be blank"}
+  #validates :ship_fee_id, numericality: { other_than: 1 , , message: "can't be blank"}
+  #validates :region_id, numericality: { other_than: 1 , , message: "can't be blank"}
+  #validates :ship_time_id, numericality: { other_than: 1 , , message: "can't be blank"}
 end
