b0a06a906b86ec2e40b791bbdb8d9ea7f151da65
akari1017 <98454251+akari1017@users.noreply.github.com>
2022-02-25 18:21:04 +0900

    バリテーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,15 @@ class Item < ApplicationRecord
   belongs_to :ship_time
   has_one_attached :image
 
+  validates :name,         presence: true
+  validates :description,  presence: true
+  validates :category_id,  presence: true
+  validates :status_id,    presence: true
+  validates :ship_fee_id,  presence: true
+  validates :region_id,    presence: true
+  validates :ship_time_id, presence: true
+  validates :price,        presence: true
+
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 , , message: "can't be blank"}
   validates :status_id, numericality: { other_than: 1 , , message: "can't be blank"}
