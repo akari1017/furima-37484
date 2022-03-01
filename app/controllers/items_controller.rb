58e1853900bdf98c9973630347bb210111dd76a4
akari1017 <98454251+akari1017@users.noreply.github.com>
2022-03-01 09:58:12 +0900

    ルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,10 +18,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Tweet.find(params[:id])
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :ship_fee_id, :region_id, :ship_time_id,
-                                 :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :ship_fee_id, :region_id, :ship_time_id,:price).merge(user_id: current_user.id)
   end
 end
