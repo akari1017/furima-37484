cf77e1e81c83aa1619a624e22f185c5443fb8ca6
akari1017 <98454251+akari1017@users.noreply.github.com>
2022-03-02 16:27:28 +0900

    formオブジェクトの作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,25 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :zip, :region_id, :city, :block, :building, :phone, :user_id, :item_id
+
+
+  validates :zip,       presence: true
+  validates :region_id, presence: true
+  validates :city,      presence: true
+  validates :block,     presence: true
+  validates :phone,     presence: true
+  validates :user_id,   presence: true
+  validates :item_id,   presence: true
+  
+  # 郵便番号は3桁ハイフン4桁での保存を許可する
+  validates :zip, format: { with: /\A\d{3}[-]\d{4}\z/, message: '郵便番号はハイフンを含めて7桁で記入してください' }
+  validates :zip, format: { with: /\A[0-9]+\z/, message: '郵便番号は半角数字で入力してください' }
+  # 電話番号は10桁以上11桁以内の半角数値のみ保存を許可する
+  validates :phone, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+  validates :price, format: { with: /\A[0-9]+\z/, message: '電話番号は半角数字で入力してください' }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(zip: zip, region_id: region_id, city: city, block: block, building: building, phone: phone, order_id: order.id)
+  end
+end
