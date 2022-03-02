63702b32f40d0876f2c41b12cac924bf43c8352f
akari1017 <98454251+akari1017@users.noreply.github.com>
2022-03-02 17:09:15 +0900

    購入記録保存の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,23 @@
 class OrdersController < ApplicationController
   
   def index
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:zip, :region_id, :city, :block, :building, :phone).merge(user_id: current_user.id)
   end
 
 end
