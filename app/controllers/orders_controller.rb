c4fd67b86a0886e67c7264c267d504ea98e4553d
akari1017 <98454251+akari1017@users.noreply.github.com>
2022-03-03 14:35:39 +0900

    コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,16 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
   
   def index
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
-    if user_signed_in? && @item.order
+    if @item.order
       redirect_to root_path
     end
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
-    @item = Item.find(params[:item_id])
     if @order_address.valid?
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
@@ -30,6 +29,10 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:zip, :region_id, :city, :block, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
