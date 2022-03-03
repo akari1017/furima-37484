37f0018336d0179d917b111d35ea59176172a96d
akari1017 <98454251+akari1017@users.noreply.github.com>
2022-03-03 18:16:59 +0900

    コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
   
   def index
     @order_address = OrderAddress.new
-    if current_user.id == @item.user_id && @item.order
+    if current_user == @item.user && @item.order
       redirect_to root_path
     end
   end
